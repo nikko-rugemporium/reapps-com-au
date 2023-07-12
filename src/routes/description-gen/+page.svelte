a496f3675b3639ea8a1ea86b35b769c623c465ad
nikko-rugemporium <nikko@rugemporium.com.au>
2023-07-12 09:25:57 +0800

    Feat: Added new page for description generator

---
+++ src/routes/description-gen/+page.svelte
@@ -0,0 +1,17 @@
+<section class="bg-white">
+      <div
+        class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
+      >
+        <div class="mx-auto text-center">
+          <h1 class="text-3xl font-bold text-neutral-800 lg:text-4xl">Product Description Generator</h1>
+         
+        </div>
+    
+        <div class="mt-16 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <input type="text" >
+    
+      </div>
+  
+      
+    </section>
+    
