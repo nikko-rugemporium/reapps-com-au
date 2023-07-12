efedb77e3b582d2db0ed0ce6bec2dc320874c744
nikko-rugemporium <nikko@rugemporium.com.au>
2023-07-12 15:04:46 +0800

    Feat: Added description component

---
+++ src/lib/description.svelte
@@ -0,0 +1,36 @@
+<script>
+
+      import "../app.postcss";
+      import StarterKit from "@tiptap/starter-kit";
+      import Placeholder from "@tiptap/extension-placeholder";
+      import { Editor } from "@tiptap/core";
+      import { onMount } from "svelte";
+
+      let element;
+      let editor;
+
+      onMount(() => {
+            editor = new Editor({
+            editorProps: {
+                  attributes: {
+                        class: 'description-box input variant-form-material block mt-10 h-56 p-3',
+                  },
+            },
+            element: element,
+            extensions: [StarterKit,
+            Placeholder.configure({
+                  placeholder: 'Text description',
+                  emptyEditorClass: 'text-primary-400',
+            })],
+            content: ``,
+            onTransaction: () => {
+            
+            editor = editor;
+            },
+            });
+      });
+</script>
+   
+    
+
+    <div bind:this={element} placeholder="Text description"></div>
