f54cef45a203606b991305d90d556966a7b81e01
nikko-rugemporium <nikko@rugemporium.com.au>
2023-07-12 12:52:21 +0800

    Update: Added product name & description input

--- src/routes/description-gen/+page.svelte
+++ src/routes/description-gen/+page.svelte
@@ -1,17 +1,24 @@
-<section class="bg-white">
+<section>
       <div
         class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
       >
         <div class="mx-auto text-center">
-          <h1 class="text-3xl font-bold text-neutral-800 lg:text-4xl">Product Description Generator</h1>
-         
+          <h1 class="text-3xl font-bold lg:text-4xl">Product Description Generator</h1>
         </div>
     
-        <div class="mt-16 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <input type="text" >
+        <div class="mt-16 flex flex-col flex-wrap gap-10">
+          <label class="label flex-1">
+            <span>Product Name</span>
+            <input type="text" class="input variant-form-material" />
+          </label>
+          <label class="label flex-1">
+            <span>Description</span>
+            <textarea class="textarea variant-form-material" rows="4" />
+          </label>
+          
     
-      </div>
+        </div>
   
       
-    </section>
+</section>
     
