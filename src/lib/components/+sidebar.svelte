b86a81b97edaaf4457ece12a0231a974f394c322
nikko-rugemporium <nikko@rugemporium.com.au>
2023-07-12 09:27:14 +0800

    Feat: Replaced custom apps & hooks link to Description Generator

--- src/lib/components/+sidebar.svelte
+++ src/lib/components/+sidebar.svelte
@@ -23,9 +23,9 @@
              </a>
           </li>
           <li>
-             <a href="/apps" class="flex items-center p-2 text-neutral-900 rounded-lg dark:text-white hover:bg-neutral-100 dark:hover:bg-neutral-700">
+             <a href="/description-gen" class="flex items-center p-2 text-neutral-900 rounded-lg dark:text-white hover:bg-neutral-100 dark:hover:bg-neutral-700">
                 <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-neutral-500 transition duration-75 dark:text-neutral-400 group-hover:text-neutral-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-                <span class="flex-1 ml-3 whitespace-nowrap">Custom Apps / Hooks</span>
+                <span class="flex-1 ml-3 whitespace-nowrap">Description Generator</span>
                 </a>
           </li>
           <li>
