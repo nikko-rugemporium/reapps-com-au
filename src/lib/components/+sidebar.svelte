d506c6052008f8ec7e175cff43b445e616b7a63c
nikko-rugemporium <nikko@rugemporium.com.au>
2023-07-13 15:03:10 +0800

    Style: Update bg color & font size

--- src/lib/components/+sidebar.svelte
+++ src/lib/components/+sidebar.svelte
@@ -10,12 +10,12 @@
     </svg>
  </button>
  
- <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+ <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-surface-200" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto ">
        <a href="https://reapps.com.au" class="flex items-center pl-2.5 mb-5">
           <img src="{ logo }" class="max-w-[140px]" alt="RE Logo" />
        </a>
-       <ul class="space-y-2 font-medium">
+       <ul class="space-y-4 font-bold text-sm">
           <li>
              <a href="/" class="flex items-center p-2 rounded-lg ">
                 <svg aria-hidden="true" class="w-6 h-6 transition duration-75 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
