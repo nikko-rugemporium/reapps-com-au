a57eb9d4d0624bd99d3e22f32a857465d2789075
nikko-rugemporium <nikko@rugemporium.com.au>
2023-07-14 16:45:03 +0800

    Style: Change copy button icon

--- src/routes/description-gen/+page.svelte
+++ src/routes/description-gen/+page.svelte
@@ -191,7 +191,7 @@
         <div class="h-1/2">
           <button type="button" class="btn variant-filled-primary" on:click={copyCode}>
             <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-code-2"><path d="M4 22h14a2 2 0 0 0 2-2V7.5L14.5 2H6a2 2 0 0 0-2 2v4"/><polyline points="14 2 14 8 20 8"/><path d="m9 18 3-3-3-3"/><path d="m5 12-3 3 3 3"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
             </span>
             <span>Copy code</span>
           </button>
