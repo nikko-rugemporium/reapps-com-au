58304e62c51bb001023e02dee202e9239f2f0065
nikko-rugemporium <nikko@rugemporium.com.au>
2023-07-18 08:42:26 +0800

    Feat: Add sidebar functionality

--- src/lib/components/+sidebar.svelte
+++ src/lib/components/+sidebar.svelte
@@ -1,17 +1,23 @@
 
 <script>
     import logo from '$lib/images/reapps-logo-black.png';
+
+    let isOpen = false;
+
+    function toggleSidebar() {
+      isOpen = !isOpen;
+    }
 </script>
 
-<button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm rounded-lg md:hidden ">
+<button on:click={ toggleSidebar } id="toggleSidebar" data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="absolute z-20 right-3 inline-flex items-center p-2 mt-2 ml-3 text-sm rounded-lg md:hidden">
     <span class="sr-only">Open sidebar</span>
     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
     </svg>
  </button>
  
- <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-surface-200" aria-label="Sidebar">
-    <div class="h-full px-3 py-4 overflow-y-auto ">
+ <aside class:open={isOpen} id="logo-sidebar" class="fixed top-0 z-10 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-surface-200" aria-label="Sidebar">
+    <div class="h-full px-3 py-4 overflow-y-auto flex flex-col justify-start">
        <a href="https://reapps.com.au" class="flex items-center pl-2.5 mb-5">
           <img src="{ logo }" class="max-w-[140px]" alt="RE Logo" />
        </a>
