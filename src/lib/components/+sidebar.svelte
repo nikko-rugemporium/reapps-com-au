37c4833137ce6fcae90592617ac96bf314ab1e6b
nikko-rugemporium <nikko@rugemporium.com.au>
2023-07-18 13:36:38 +0800

    Feat: Add close sidebar on page switch

--- src/lib/components/+sidebar.svelte
+++ src/lib/components/+sidebar.svelte
@@ -1,14 +1,40 @@
 
 <script>
-    import logo from '$lib/images/reapps-logo-black.png';
-    import { page } from '$app/stores';
-    $: activeUrl = $page.url.pathname;
+   import logo from '$lib/images/reapps-logo-black.png';
+   import { page } from '$app/stores';
+   import { onMount, onDestroy } from "svelte";
+   import { writable } from 'svelte/store';
 
-    let isOpen = false;
+   let initialRoute = writable('');
+   let prevRoute = '';
+   let activeUrl = '';
+   let isOpen = false;
 
-    function toggleSidebar() {
+   function storeValue() {
+      initialRoute.set(prevRoute);
+      prevRoute = activeUrl;
+      activeUrl = $page.url.pathname;
+
+      // Close the sidebar when switching to a new page
+      isOpen = false;
+   }
+
+   // Subscribe to the $page store
+   const unsubscribe = page.subscribe(storeValue);
+
+   onMount(() => {
+      prevRoute = $page.url.pathname;
+      activeUrl = $page.url.pathname;
+      storeValue();
+   });
+
+   onDestroy(() => {
+      unsubscribe();
+   });
+
+   function toggleSidebar() {
       isOpen = !isOpen;
-    }
+   }
 </script>
 
 <button on:click={ toggleSidebar } id="toggleSidebar" data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="fixed bg-surface-50 border border-surface-500 z-20 right-3 inline-flex items-center p-2 mt-2 ml-3 text-sm rounded-lg md:hidden">
@@ -16,48 +42,48 @@
     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
        <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
     </svg>
- </button>
+</button>
  
- <aside class:open={isOpen} id="logo-sidebar" class="fixed top-0 z-10 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-surface-200" aria-label="Sidebar">
-    <div class="h-full px-3 py-4 overflow-y-auto flex flex-col justify-start">
-       <a href="https://reapps.com.au" class="flex items-center pl-2.5 mb-5">
-          <img src="{ logo }" class="max-w-[140px]" alt="RE Logo" />
-       </a>
-       <ul class="space-y-4 mt-3 md:mt-0 font-bold text-sm">
-          <li class:active={activeUrl === '/'}>
-             <a href="/" class="flex items-center p-2 ">
-                <svg aria-hidden="true" class="w-6 h-6 transition duration-75 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
-                <span class="ml-3">Home</span>
-             </a>
-          </li>
-          <li class:active={activeUrl === '/description-gen'}>
-             <a href="/description-gen" class="flex items-center p-2 ">
-                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 xt-neutral-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-                <span class="ml-3">Description Generator</span>
-                </a>
-          </li>
-          <li class:active={activeUrl === '/about'}>
-             <a href="/about" class="flex items-center p-2 ">
-                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                <span class="ml-3">About</span>
+<aside class:open={isOpen} id="logo-sidebar" class="fixed top-0 z-10 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-surface-200" aria-label="Sidebar">
+   <div class="h-full px-3 py-4 overflow-y-auto flex flex-col justify-start">
+      <a href="https://reapps.com.au" class="flex items-center pl-2.5 mb-5">
+         <img src="{ logo }" class="max-w-[140px]" alt="RE Logo" />
+      </a>
+      <ul class="space-y-4 mt-3 md:mt-0 font-bold text-sm">
+         <li class:active={activeUrl === '/'}>
+            <a href="/" class="flex items-center p-2">
+               <svg aria-hidden="true" class="w-6 h-6 transition duration-75 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
+               <span class="ml-3">Home</span>
             </a>
-          </li>
-          <li class:active={activeUrl === '/documentation'}>
-             <a href="/documentation" class="flex items-center p-2 ">
-                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
-                <span class="ml-3">Documentation</span>
-             </a>
-          </li>
-          <li>
-             <a href="https://rugemporium.com.au/" target="_blank" class="flex items-center p-2 ">
-                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
-                <span class="ml-3">RE Store</span>
-             </a>
-          </li>
-          
-       </ul>
-    </div>
- </aside>
+         </li>
+         <li class:active={activeUrl === '/description-gen'}>
+            <a href="/description-gen" class="flex items-center p-2 ">
+               <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 xt-neutral-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
+               <span class="ml-3">Description Generator</span>
+               </a>
+         </li>
+         <li class:active={activeUrl === '/about'}>
+            <a href="/about" class="flex items-center p-2">
+               <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+               <span class="ml-3">About</span>
+         </a>
+         </li>
+         <li class:active={activeUrl === '/documentation'}>
+            <a href="/documentation" class="flex items-center p-2">
+               <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
+               <span class="ml-3">Documentation</span>
+            </a>
+         </li>
+         <li>
+            <a href="https://rugemporium.com.au/" target="_blank" class="flex items-center p-2">
+               <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
+               <span class="ml-3">RE Store</span>
+            </a>
+         </li>
+         
+      </ul>
+   </div>
+</aside>
  
  
  
