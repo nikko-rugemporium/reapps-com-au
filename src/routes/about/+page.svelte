039b938535fefa5d0bd5a2f6383b3ef7c37f00ca
nikko-rugemporium <nikko@rugemporium.com.au>
2023-07-19 14:16:51 +0800

    Feat: Add content for About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,127 @@
-<h1>About here..</h1>
+<script lang="ts">
+      import Nikko from "$lib/images/nikko-dp.png";
+      import Placeholder from "$lib/images/placeholder-dp.png";
+</script>
+
+<section class="flex flex-wrap gap-x-10">
+      <div class="px-4 py-8 sm:py-12 sm:px-6 lg:py-4 lg:px-8 flex flex-col w-full mb-10" >
+            <div>
+            <h1 class="text-3xl font-bold lg:text-2xl pb-2">About RE Apps</h1>
+            </div>
+            <p>A dedicated internal website that simplifies the process of managing the Rug Emporium Shopify store. </p>
+      </div>
+
+      <div class="px-4 sm:px-6 lg:px-8 flex flex-col gap-10 w-full" >
+            <div >
+            <h1 class="text-3xl font-bold lg:text-2xl pb-2">RE Team</h1>
+            </div>
+      </div>
+      <div class="w-full flex flex-wrap gap-4 px-4 py-2 sm:py-4 sm:px-6 lg:py-4 lg:px-8" >
+
+            <div class="card flex flex-wrap px-2 py-10 sm:py-10 md:py-10 lg:py-8 xl:py-6 bg-initial card-hover overflow-hidden w-full xl:w-5/12 gap-4 justify-center items-center">
+                  <header class="w-32 text-center flex flex-col items-center gap-3">
+                        <img class="max-w-[100px] rounded-full" src="{ Placeholder }" alt="Name">
+                        <span class="font-bold text-sm" >Name</span>  
+                  </header> 
+                  <div class="p-4 space-y-4 w-3/4">
+                        <article>
+                              <p class="text-sm leading-relaxed">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae massa pellentesque augue tincidunt tempor in ac lectus. Nam vitae tortor volutpat enim auctor interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                              </p>
+                        </article>
+                        <span class="badge variant-form-material">Sample #1</span>
+                        <span class="badge variant-form-material">Sample #2</span>
+                        <span class="badge variant-form-material">Sample #3</span>
+                  </div> 
+            </div>
+
+            <div class="card flex flex-wrap px-2 py-10 sm:py-10 md:py-10 lg:py-8 xl:py-6 bg-initial card-hover overflow-hidden w-full xl:w-5/12 gap-4 justify-center items-center">
+                  <header class="w-32 text-center flex flex-col items-center gap-3">
+                        <img class="max-w-[100px] rounded-full" src="{ Placeholder }" alt="Name">
+                        <span class="font-bold text-sm" >Name</span>  
+                  </header> 
+                  <div class="p-4 space-y-4 w-3/4">
+                        <article>
+                              <p class="text-sm leading-relaxed">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae massa pellentesque augue tincidunt tempor in ac lectus. Nam vitae tortor volutpat enim auctor interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                              </p>
+                        </article>
+                        <span class="badge variant-form-material">Sample #1</span>
+                        <span class="badge variant-form-material">Sample #2</span>
+                        <span class="badge variant-form-material">Sample #3</span>
+                  </div> 
+            </div>
+
+            <div class="card flex flex-wrap px-2 py-10 sm:py-10 md:py-10 lg:py-8 xl:py-6 bg-initial card-hover overflow-hidden w-full xl:w-5/12 gap-4 justify-center items-center">
+                  <header class="w-32 text-center flex flex-col items-center gap-3">
+                        <img class="max-w-[100px] rounded-full" src="{ Placeholder }" alt="Name">
+                        <span class="font-bold text-sm" >Name</span>  
+                  </header> 
+                  <div class="p-4 space-y-4 w-3/4">
+                        <article>
+                              <p class="text-sm leading-relaxed">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae massa pellentesque augue tincidunt tempor in ac lectus. Nam vitae tortor volutpat enim auctor interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                              </p>
+                        </article>
+                        <span class="badge variant-form-material">Sample #1</span>
+                        <span class="badge variant-form-material">Sample #2</span>
+                        <span class="badge variant-form-material">Sample #3</span>
+                  </div> 
+            </div>
+
+            <div class="card flex flex-wrap px-2 py-10 sm:py-10 md:py-10 lg:py-8 xl:py-6 bg-initial card-hover overflow-hidden w-full xl:w-5/12 gap-4 justify-center items-center">
+                  <header class="w-32 text-center flex flex-col items-center gap-3">
+                        <img class="max-w-[100px] rounded-full" src="{ Placeholder }" alt="Name">
+                        <span class="font-bold text-sm" >Name</span>  
+                  </header> 
+                  <div class="p-4 space-y-4 w-3/4">
+                        <article>
+                              <p class="text-sm leading-relaxed">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae massa pellentesque augue tincidunt tempor in ac lectus. Nam vitae tortor volutpat enim auctor interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                              </p>
+                        </article>
+                        <span class="badge variant-form-material">Sample #1</span>
+                        <span class="badge variant-form-material">Sample #2</span>
+                        <span class="badge variant-form-material">Sample #3</span>
+                  </div> 
+            </div>
+
+            <div class="card flex flex-wrap px-2 py-10 sm:py-10 md:py-10 lg:py-8 xl:py-6 bg-initial card-hover overflow-hidden w-full xl:w-5/12 gap-4 justify-center items-center">
+                  <header class="w-32 text-center flex flex-col items-center gap-3">
+                        <img class="max-w-[100px] rounded-full" src="{ Placeholder }" alt="Name">
+                        <span class="font-bold text-sm" >Name</span>  
+                  </header> 
+                  <div class="p-4 space-y-4 w-3/4">
+                        <article>
+                              <p class="text-sm leading-relaxed">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae massa pellentesque augue tincidunt tempor in ac lectus. Nam vitae tortor volutpat enim auctor interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                              </p>
+                        </article>
+                        <span class="badge variant-form-material">Sample #1</span>
+                        <span class="badge variant-form-material">Sample #2</span>
+                        <span class="badge variant-form-material">Sample #3</span>
+                  </div> 
+            </div>
+
+            <div class="card flex flex-wrap px-2 py-10 sm:py-10 md:py-10 lg:py-8 xl:py-6 bg-initial card-hover overflow-hidden w-full xl:w-5/12 gap-4 justify-center items-center">
+                  <header class="w-32 text-center flex flex-col items-center gap-3">
+                        <img class="max-w-[100px] rounded-full" src="{ Nikko }" alt="Nikko">
+                        <span class="font-bold text-sm" >Nikko Parin</span>  
+                  </header> 
+                  <div class="p-4 space-y-4 w-3/4">
+                        <article>
+                              <p class="text-sm leading-relaxed">
+                              As a passionate learner, I thrive on expanding my knowledge and skills, particularly in the realms of web development and design. When I'm not immersed in my work, you can find me indulging in my love for PC gaming, with games like Valorant.
+                              <i>Thanks ChatGPT!</i>
+                              </p>
+                        </article>
+                        <span class="badge variant-form-material">Introvert</span>
+                        <span class="badge variant-form-material">Dog Lover</span>
+                        <span class="badge variant-form-material">Gamer</span>
+                  </div> 
+            </div>
+
+
+      </div>
+
+</section>
